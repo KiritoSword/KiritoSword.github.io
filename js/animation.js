017282b61d5f7bd19344f5ef8cd8ee79ef6fc1c5
KiritoSword <zakkjpn@protonmail.com>
2020-11-08 23:43:00 +0900

    updated

---
+++ js/animation.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal()
+
+sr.reveal(".navbar", {
+    delay:300,
+    opacity:1,
+    origin:"top",
+    distance:"100%",
+    duration:1000
+});
+
+sr.reveal(".top-section", {
+    delay:300,
+    opacity:1,
+    origin:"right",
+    distance:"100%",
+    duration:2500
+});
+
+sr.reveal(".about-section h1", {
+    delay:300,
+    duration:1500
+});
+
+sr.reveal(".about-section ul", {
+    delay:700,
+    origin:"bottom",
+    distance:"50%",
+    duration:1000
+});
+
+sr.reveal(".contact-section ul li a img", {
+    delay:300,
+    scale:0,
+    duration:2000
+});
